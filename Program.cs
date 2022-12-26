509f51ba836399baf74037d8ceabf54a09ce2c41
Феденко Олег Алексеевич <o.fedenko@2gis.ru>
2022-12-26 09:12:49 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+         int i = 1;
+         bool not = true;
+            Console.WriteLine("Введите число:");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Чётные числа от 1 до " + number);
+            while (i <= number)
+            {
+                if (i % 2 != 1)
+                {
+                    Console.Write(i + ", ");
+                    not = false;
+                }
+                i++;
+            }
+             if (not)
+            {
+                Console.WriteLine("Нет чётных чисел!");
+            }
